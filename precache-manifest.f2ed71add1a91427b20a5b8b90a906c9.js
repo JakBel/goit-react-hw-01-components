05dbcd2be7685cf0ba1ae2a4538ccc1bd984d19e
JakBel <73158151+JakBel@users.noreply.github.com>
2021-11-25 18:59:06 +0100

    Updates

---
+++ precache-manifest.f2ed71add1a91427b20a5b8b90a906c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5e8bce71c3124fb0848f1e15b16b6c",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "9618d12643d812eb152f",
+    "url": "/goit-react-hw-01-components/static/css/main.933b88b1.chunk.css"
+  },
+  {
+    "revision": "b1c5e685e7478249771b",
+    "url": "/goit-react-hw-01-components/static/js/2.1b9c6bc1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/goit-react-hw-01-components/static/js/2.1b9c6bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac3086cbcfabe988f39",
+    "url": "/goit-react-hw-01-components/static/js/3.bc9ed203.chunk.js"
+  },
+  {
+    "revision": "9618d12643d812eb152f",
+    "url": "/goit-react-hw-01-components/static/js/main.9fb181e5.chunk.js"
+  },
+  {
+    "revision": "d101fa6ce29324dc9ffb",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.cc379b13.js"
+  }
+]);
